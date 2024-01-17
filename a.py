87b2c5fdb8be29f8019d02756279bf848896deaf
hieupri <nkth.privacy@gmail.com>
2024-01-17 15:10:38 +0700

    #4 commit for 2024-01-17

--- a.py
+++ a.py
@@ -5,29 +5,20 @@ import time  # Import thư viện time để sử dụng hàm sleep
 
 try:
     count = 0
+    start_date = datetime.date(1, 1, 1) 
+    end_date = datetime.date(2024, 1, 1)
+
     while True:  # Vòng lặp vô hạn
-        today = datetime.date.today()
+        res_date = start_date
 
-        current_year = today.year
-        current_month = today.month
-        current_day = today.day
-
-        last_year = current_year - 1
-        last_month = today.month
-        last_day = current_day - 1  # In case it is a leap year
-
-        start = datetime.date(last_year, last_month, last_day)
-        end = datetime.date(current_year, current_month, current_day)
-        res_date = start
-
-        while res_date <= end:
+        while res_date < end_date:
             for i in range(random.randrange(1, 6)):
                 with open('change-file.txt', 'a') as wf:
                     wf.write(f'\n{res_date}')
                 os.system(f'git add .')
                 os.system(f'git commit --date "{res_date}" -m "#{i} commit for {res_date}"')
                 count += 1
-                if count >= 5000:
+                if count >= 5000000:
                     raise StopIteration  # Raise ngoại lệ để thoát khỏi vòng lặp
             res_date += datetime.timedelta(days=1)
 
